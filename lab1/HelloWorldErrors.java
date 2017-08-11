a8804766ce95cb99eefb113259def6a2d60202c6
Daniel Graham <dggraham@cs.wm.edu>
2017-08-11 12:35:11 -0400

    Added HelloWorldError.java file

---
+++ lab1/HelloWorldErrors.java
@@ -0,0 +1,6 @@
+public class HelloWorldErrors {
+    public static void main(String[] argvs) {
+            String a = CS105"
+	            SyStem.outprintln("Hello Bridgewater "a + "!");
+		        {
+			
