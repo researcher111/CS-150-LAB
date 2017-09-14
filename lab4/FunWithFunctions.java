b9867bff3a975321498d6596dff6962d879c5086
Daniel Graham <researcher111@users.noreply.github.com>
2017-09-14 13:15:28 -0400

    Update FunWithFunctions.java

--- lab4/FunWithFunctions.java
+++ lab4/FunWithFunctions.java
@@ -2,13 +2,13 @@
 
 public class FunWithFunctions {
 	//Step 1: Add the two integer parameters 
-	public int divide(     ) {
+	public static int divide(     ) {
 		//Step 2: Copy your divide code here and update
 		//Step 3: Instead of printing answer return value
 		return 0;
 	} 
 	
-	public void main() {
+	public static void main() {
 		int a = 30;
 		int b = 40;
 		//Step 4: Updating 
