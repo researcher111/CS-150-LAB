831753eba1ea19659afdfe6420e1860d27ac2f02
Daniel Graham <researcher111@users.noreply.github.com>
2017-09-14 16:15:50 -0400

    Update DoubleForLoops.java

--- lab3/DoubleForLoops.java
+++ lab3/DoubleForLoops.java
@@ -4,7 +4,7 @@ public class DoubleForLoops {
 	public static void main(String[] args) {
 		for(int i =1; i <= 2; i++) {
 			for(int j =0; j <= 9; j++) {
-				System.out.println("i  j"+" " + i " " + j);
+				System.out.println("i  j"+" " + i +" " + j);
 			}
 		}
 	}
